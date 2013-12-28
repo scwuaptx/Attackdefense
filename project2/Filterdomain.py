155ef3f01a72dab9554fd33d9a23b77004aef47c
Angelboy <scwuaptx@gmail.com>
2013-12-28 14:24:59 +0800

    add project2

---
+++ project2/Filterdomain.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#The program will filiter the domain which forward ip less than 4
+#Source file : list   Result file : result 
+
+import os
+import popen2
+
+#NumberOFIP() check the number of ip which is forwarded by the domain
+def NumberOfIP(domain):
+	IP,DataIn = popen2.popen2("host -t A "+domain)
+	SeparateIP = (IP.read()).split("\n")
+	number = len(SeparateIP)
+
+	return number
+
+if __name__=='__main__':
+	fileopen = open('result','w')
+	for domain in open('list'):  
+		if NumberOfIP(domain) > 4:
+			fileopen.write(domain)
+	fileopen.close()
