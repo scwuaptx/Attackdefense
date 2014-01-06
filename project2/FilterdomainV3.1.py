887e327def7756b5d943eaba10ce19a98901558b
Angelboy <scwuaptx@gmail.com>
2014-01-07 02:02:26 +0800

    Add detect of different country

---
+++ project2/FilterdomainV3.1.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#The program will filiter the domain,which forward ip less than 4,then record it in the goodresult
+#If the asn of ip are different ,then record it in the badresult
+#Source file : list   Result file : goodresult and badreuslt
+#if IP > 4 and ASN > 1 and not cdn than record in badresult
+#if IP > 4 but ASN == 1 or is cdn than record in goodresult
+
+import os
+import popen2
+
+
+#DomainToIP() return ip list which is forwarded by the domain
+def DomainToIP(domain):
+	IPlist=[]
+	domain = "".join(domain.split())
+	for dns in [" "," 139.175.55.244"," 8.8.8.8"]:
+		DataOut,DataIn = popen2.popen2("host -t A " + domain + dns)
+		SeparateIP = DataOut.readlines()
+		for i in range (len(SeparateIP)-1):
+			temp = SeparateIP[i].split()
+			if len(temp) > 0 :
+				try :
+					IP = temp[3]
+					if IP not in "alias" :
+						if IP not in IPlist :
+							IPlist.append(IP)
+				except:
+					continue
+
+ 	return IPlist
+
+#NumberOfAsn() return the asn which is forwarded by the IP 
+def IPtoASN(IPlist):
+	ASN = []
+	for IP in IPlist :
+		try :
+			ASNOut,IPIN = popen2.popen2("whois -h whois.cymru.com "+IP)		
+			SeparateASN = (ASNOut.read()).split("\n")
+			temp = SeparateASN[1].split()
+			if temp[0] not in ASN :
+				ASN.append(temp[0])
+		except :
+			continue
+		
+	return ASN	
+
+#TryCDN will check the domain whether belong to cdn.
+def TryCDN(domain):
+	CDN = ["akamai","cdn","akafms","amazon","google","adobe"]
+	DataOut,DataIn = popen2.popen2("host -t NS " + domain)
+	SeparateIP = (DataOut.read()).split(".")
+	for i in CDN :
+		if i in SeparateIP :
+			return True
+	return False
+
+if __name__=='__main__':
+	fileopengood = open('goodresult','w')
+	fileopenbad = open('badresult','w')	
+	for domain in open('list'):  
+		IPlist = DomainToIP(domain)
+		if len(IPlist) > 4 :
+			if not TryCDN(domain) and len(IPtoASN(IPlist)) > 1 :
+				fileopenbad.write(domain)
+			else :
+				fileopengood.write(domain)
+				
+	fileopengood.close()
+	fileopenbad.close()
+
